587d3e9ea4d66be1a701a5f114603f36fdb1cb6a
Fabiano Monteiro <fabianomonteiro@outlook.com>
2022-03-23 02:13:38 -0300

    Inclusão dos arquivos

---
+++ src/04-Clean Architecture/FinanceImportAutomator/02-Domain/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceImportAutomator._02_Domain
+{
+    internal class Transaction
+    {
+    }
+}
---
+++ src/04-Clean Architecture/FinanceImportAutomator/03-Infra/GetCategoryQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceImportAutomator._03_Infra
+{
+    internal class GetCategoryQuery
+    {
+    }
+}
